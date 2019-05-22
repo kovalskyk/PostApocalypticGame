dae07112c4ec07ba76ab3cc2478a9107520d5562
Kristián Kovalský <k.kovalsky@me.com>
2019-05-22 10:41:38 +0200

    all of the game
    in it's entirety

---
+++ Corridors/Assets/FindingObject.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class FindingObject : MonoBehaviour
+{
+    public Text myText;
+    private int score;
+
+    AudioSource pickUpSound;
+
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Screen.lockCursor = true;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "axet")
+        {
+            pickUpSound = col.GetComponent<AudioSource>();
+            pickUpSound.Play();
+            Debug.Log("Collision");
+            score++;
+            Destroy(col.gameObject, 0.4f);
+            myText.text = "Axes collected: " + score + "/4";
+        }
+
+        if (score == 4 & (SceneManager.GetActiveScene().name == "FinalScene"))
+        {
+            System.Threading.Thread.Sleep(1000);
+            SceneManager.LoadScene("Scene2");
+            score = 0;
+        }
+        else if (score == 4 & (SceneManager.GetActiveScene().name == "Scene2"))
+        {
+            myText.text = "Game completed!";
+        }
+    }
+}
---
+++ Corridors/Assets/Flashlight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    private Light myLight;
+
+    private void Start()
+    {
+        myLight = this.GetComponent<Light>();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+}
